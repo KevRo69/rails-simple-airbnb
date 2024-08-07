7e0e7f2f7a67937510c02b9738ccb087630dee19
kevin rossi <kevinrossi@free.fr>
2024-08-07 16:35:03 +0200

    Scaffold Flat and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+Flat.destroy_all
+puts "Generating 4 flats"
+
+4.times do
+  flat = Flat.new(
+    name: Faker::Superhero.name,
+    address: Faker::Address.full_address,
+    description: Faker::Quote.most_interesting_man_in_the_world,
+    price_per_night: (0..200).to_a.sample,
+    number_of_guests: (1..6).to_a.sample
+  )
+  flat.save!
+end
+puts "finished"
